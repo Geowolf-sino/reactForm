990a09fd17be821fc025d148543e533a7b184e71
Nuradil <i.jackreacher@mail.ru>
2023-06-29 16:39:35 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+return (
+  <div >
+  <h1>List item</h1>
+</div>
+)
+  
+}
+
+export default Header;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import Users from "./Users";
+import Header from "./Header";
+import '../style/main.css';
+import AddUser from "./AddUser";
+
+const HomePage = () => {
+
+  const [users, setUsers] = useState([]);
+  const [deleteUserId, setDeleteUserId] = useState([]);
+  // const [editUserForm, setEditUserForm] = useState([]);
+
+  useEffect(() => {
+    const userList = [
+      {
+        id: 1,
+        firstName: 'Bob',
+        lastName: 'Marley',
+        bio: 'This will start the development server and open your application in a web browser. You can now begin working on your React project',
+        age: 40,
+        isHappy: true
+      },
+      {
+        id: 2,
+        firstName: 'John',
+        lastName: 'Doe',
+        bio: 'Congratulations! You have successfully created a new React project using Create React App. You can now explore and modify the proje',
+        age: 22,
+        isHappy: false
+      }
+    ];
+    setUsers([...userList]);
+
+    return () => {
+
+    };
+  }, []);
+
+  const addNewUser = (newUser) => {
+    const id  = users.length + 1;
+    setUsers([...users, {id, ...newUser}])
+  }
+
+useEffect(() => {
+    if(deleteUserId !== null){
+      setUsers((prevUsers) => 
+      prevUsers.filter((user)=> user.id !== deleteUserId)
+      );
+      setDeleteUserId(null);
+    }
+  },[deleteUserId])
+
+const deleteUser = (id) => {
+setDeleteUserId(id)
+} 
+
+const editUser = (user) => {
+console.log(user);
+}
+
+
+
+  return (
+    <div>
+      <header className="header">
+        <Header />
+      </header>
+
+
+      <main>
+        <Users users={users} onDelete={deleteUser} onEdit={editUser}/>
+      </main>
+      <aside>
+        <AddUser {...{ addNewUser }} />
+
+      </aside>
+    </div>
+  )
+}
+
+
+
+export default HomePage;
